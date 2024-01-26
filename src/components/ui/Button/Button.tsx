dfcd9eae63267b29c57a2e36b6c8de0507a30d45
Ruslan Mingaleev <neoch56@mail.ru>
2024-01-26 17:51:37 +0300

    Add Button component

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,49 @@
+import type { LinkProps } from 'next/link'
+import type { UrlObject } from 'url'
+import { FC, ButtonHTMLAttributes, ComponentType } from 'react'
+import cn from 'classnames'
+import Link from 'next/link'
+import Loader from '../Loader/Loader'
+
+import styles from './button.module.scss'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  href?: string | UrlObject
+  as?: ComponentType<LinkProps>
+  isLoading?: boolean
+}
+
+const Button: FC<ButtonProps> = ({
+  className,
+  children,
+  as,
+  href,
+  isLoading = false,
+  ...props
+}) => {
+  if (as === Link) {
+    if (!href) {
+      throw new Error('The "href" prop is required when using Link component.')
+    }
+
+    return (
+      <Link className={cn(className, styles.link)} href={href}>
+        {children}
+      </Link>
+    )
+  }
+
+  return (
+    <button className={cn(className, styles.button)} {...props}>
+      {isLoading ? (
+        <>
+          Загрузка <Loader className={styles.loader} />
+        </>
+      ) : (
+        children
+      )}
+    </button>
+  )
+}
+
+export default Button
