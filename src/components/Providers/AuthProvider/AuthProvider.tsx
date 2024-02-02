77ca680a82f57d7379df57ae875c522ebf018850
Ruslan Mingaleev <neoch56@mail.ru>
2024-02-02 18:25:25 +0300

    Add AuthProvider

---
+++ src/components/Providers/AuthProvider/AuthProvider.tsx
@@ -0,0 +1,47 @@
+import { type FC, type PropsWithChildren, useEffect } from 'react'
+import { signOut, useSession } from 'next-auth/react'
+import { usePathname, useRouter } from 'next/navigation'
+import { UserRole } from '@/types/auth.types'
+import { jwtDecode } from 'jwt-decode'
+import { toast } from 'react-toastify'
+
+const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
+  const router = useRouter()
+  const pathname = usePathname()
+  const { data: session, status } = useSession()
+
+  const checkTokenExpire = (token: string) => {
+    const { exp } = jwtDecode<{ exp: number }>(token)
+    const expDate = new Date(exp * 1000)
+    const nowDate = new Date()
+
+    return expDate < nowDate
+  }
+
+  useEffect(() => {
+    if (status === 'loading' || status === 'unauthenticated') return
+
+    if (!session || !session.user) return
+
+    const { user } = session
+
+    if (checkTokenExpire(user.accessToken)) {
+      signOut({ redirect: false })
+      toast.info('Истекло время сессии. Войдите в аккаунт чтобы продолжить.')
+      router.push('/auth/signin')
+    }
+
+    if (pathname === '/') {
+      if (user.role === UserRole.Student) router.push('/profile')
+
+      if (user.role === UserRole.Admin || user.role === UserRole.Moderator)
+        router.push('/manage')
+    }
+
+    return
+  }, [session, status, pathname])
+
+  return children
+}
+
+export default AuthProvider
--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -1,9 +1,14 @@
 'use client'
 import type { FC, PropsWithChildren } from 'react'
 import { SessionProvider } from 'next-auth/react'
+import AuthProvider from './AuthProvider/AuthProvider'
 
 const Providers: FC<PropsWithChildren> = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>
+  return (
+    <SessionProvider>
+      <AuthProvider>{children}</AuthProvider>
+    </SessionProvider>
+  )
 }
 
 export default Providers
